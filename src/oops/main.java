7b2e5cd5e3442bcff1949727f3238255bde8a873
Naman Kaushik <kaushiknaman68@gmail.com>
2024-08-08 23:55:19 +0530

    Update main.java

--- src/oops/main.java
+++ src/oops/main.java
@@ -1,7 +1,9 @@
 package oops;
 
 class Car {
-	public void opendoor() {
+	
+	public void opendoor() 
+	{
 		System.out.println("Opening the Door");
 	}
 	public void closedoor() {
@@ -10,7 +12,18 @@ class Car {
 }
 
 public class main {
+	
+	int x;
+	String name="Naman";
+	
 public static void main(String[]args) {
+	
+	main m1= new main();
+	m1.x=10;
+	m1.name="Harsh";
+	System.out.println(m1.name);
+	System.out.println(m1.x);
+	
 	
 	Car c1 = new Car();
 	c1.opendoor();
